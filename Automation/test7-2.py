6f8fa6c4ff7cbc636b5f672790acc83a4f50eb33
Playone <safeyang@gmail.com>
2017-10-20 09:48:52 +0800

    commit for automation pratice

---
+++ Automation/test7-2.py
@@ -0,0 +1,25 @@
+#encoding: utf-8
+
+import urllib2
+from bs4 import BeautifulSoup as bs
+import csv
+from datetime import datetime
+
+quote_page = [
+    'http://www.bloomberg.com/quote/SPX:IND',
+    'http://www.bloomberg.com/quote/CCMP:IND'
+]
+data = []
+
+for pg in quote_page:
+    page = urllib2.urlopen(pg)
+    soup = bs(page, 'html.parser')
+    namebox = soup.find('h1', attrs={'class': 'name'})
+    name = namebox.text.strip()
+    pricebox = soup.find('div', attrs={'class': 'price'})
+    price = pricebox.text.strip()
+    data.append((name, price)) # 把提取出來的name和price 放到data
+    with open('index2.csv', 'a') as csv_file:
+        writer = csv.writer(csv_file)
+        for name, price in data: # 用for loop把data裡面的資料寫入csv
+            writer.writerow([name, price, datetime.now()])
