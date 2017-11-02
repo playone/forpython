5a4c3b140ef72fa8c7781821da2b879d704b3581
Playone <safeyang@gmail.com>
2017-11-02 11:17:32 +0800

    Newbie pratice

--- Newbie/test17.py
+++ Newbie/test17.py
@@ -15,6 +15,8 @@ count = 0
 n = input("請輸入整數：")
 for move in hanoi(int(n), 'A', 'B', 'C'):
     count += 1
+    print ('第 %i 步' %count)
     print("盤由 %c 移至 %c" % move)
+
 
 print ('總步數: %i' %count )
