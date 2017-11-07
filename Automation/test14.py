fd8e69ba6fbd0e4fcee507a73f52b1ef74b60ccc
Playone <safeyang@gmail.com>
2017-11-07 13:59:06 +0800

    For pratice automation

--- Automation/test14.py
+++ Automation/test14.py
@@ -6,9 +6,9 @@ F3是資料 F4是關鍵字 F5是結果
 利用F4 去搜尋F3 將結果存至F5
 """
 
-with open('3.txt') as f3, open('4.txt') as f4, open('result.txt', 'a') as f5:
-    a = [line.strip() for line in f4.readlines()]
-    for li in f3.readlines():
+with open('3.txt') as f3, open('4.txt') as f4, open('result.txt', 'w') as f5:
+    a = [line.strip() for line in f4]
+    for li in f3:
         new_line = li.strip()
         for i in a:
             if i in new_line:
