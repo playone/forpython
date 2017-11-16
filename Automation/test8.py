b4e659895177b99b21b10a86c2efe29891e33537
Playone <safeyang@gmail.com>
2017-11-16 11:04:11 +0800

    For pratice automation

--- Automation/test8.py
+++ Automation/test8.py
@@ -12,10 +12,10 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-page = int(input("請輸入要擷取的頁數"))
+page = int(input("請輸入要擷取的頁數:"))
 for i in range(1,page+1):
     res = requests.get("https://www.mobile01.com/forumtopic.php?c=17&p="+ str(i))
-    soup = BeautifulSoup(res.text,"html.parser", from_encoding='UTF-8')
+    soup = BeautifulSoup(res.text,"html.parser")
 
     for title in soup.select(".topic_gen"):
         print ("==============")
