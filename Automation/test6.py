0f04fae613c28858ebdbaf93366034a42a4faf7c
Playone <safeyang@gmail.com>
2017-12-04 09:46:59 +0800

    For pratice automation

--- Automation/test6.py
+++ Automation/test6.py
@@ -16,8 +16,8 @@ df = pd.read_excel('keywords.xls') #將excel 內容轉存成dataframework
 driver = webdriver.Chrome()
 driver.get('http://www.baidu.com')
 
-search_result = list(df.iloc[:, 1]) #將dataframework的第二欄放到search_result, 下面會將搜尋完成的結果放到這裡
-
+search_result = list(df.iloc[:, 1]) #宣告變數，將dataframework的第二欄放到search_result, 下面會將搜尋完成的結果放到這裡
+#print df
 def search_keyword(word):
     driver.find_element_by_id('kw').clear()
     driver.find_element_by_id('kw').send_keys(word)
@@ -27,12 +27,16 @@ def search_keyword(word):
     result = driver.find_element_by_xpath('//*[@id="2"]/h3/a[1]')
     return result.text #定義函式, 搜尋關鍵字並將文字結果回傳出來
 
-for index, word in enumerate(list(df.iloc[:, 0])): #此for loop, index對應到 df.iloc的列, word對應到欄
-    search_result[index] = search_keyword(word) #呼叫函式運行, 並將結果回傳給list的第二欄
+for index, word in enumerate(list(df.iloc[:, 0])):
+    '''
+    先用list將excel裡面的第一欄位的關鍵字列出來，再用enumerate 組成索引序列
+    index對應的就是索引序列的index, word則是索引序列裡面的值
+    '''
+    search_result[index] = search_keyword(word) #呼叫函式運行, 利用word做關鍵字搜尋，再將結果回放到search_result
     #print search_result
 
-df.iloc[:, 1] = search_result #list存成dataframework
+df.iloc[:, 1] = search_result #回存到pandas 的dataframework
 
-df.to_excel('search_result.xls', index = False) #存檔
+df.to_excel('search_result.xls', index = False) #存檔。 index=faulse 表示不需寫入index到excel
 
 driver.quit()
